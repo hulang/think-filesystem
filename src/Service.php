2dbae488fe05f70ef93481251db590048e3dfd57
快乐浪子哥 <4961831+hulang@users.noreply.github.com>
2024-12-03 16:55:31 +0800

    Update

--- src/Service.php
+++ src/Service.php
@@ -6,6 +6,12 @@ namespace hulang\filesystem;
 
 class Service extends \think\Service
 {
+    /**
+     * 注册文件系统服务
+     *
+     * 该方法通过绑定 'filesystem' 抽象到 Filesystem 类来注册文件系统服务
+     * 这使得文件系统服务可以在应用程序中通过依赖注入来使用
+     */
     public function register()
     {
         $this->app->bind('filesystem', Filesystem::class);
